ff11c9c5d79e28993daab1aab6aa6bef40781786
nico <nico.xu@highdatas.com>
2022-04-07 11:28:23 +0800

    增加自定义配置项示例

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value( "${test.hello:TEST}" )
+    private String testHello;
+
 //    @RequestMapping("/hello")
     @GetMapping("/hello")
     public String hello(){
-        return "hello word";
+        return "hello word" +testHello;
     }
     @PostMapping("/hello/post")
     public String helloPost(String name){
